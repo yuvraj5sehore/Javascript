43a125291db42d4dbdcf2a5b9cf54c8836a33965
yuvraj singh thakur <yuvraj5sehore@gmail.com>
2026-08-24 21:58:01 +0530

    Add files via upload

--- functions/functions.js
+++ functions/functions.js
@@ -1,5 +1,11 @@
 //functions in js is the reusable blocks of code designed to perform a specific task.
 
+
+// function <identifer>(<parameter a>,<parameter b>,...) {
+// <function body>
+// }
+
+
 function name(value){ //value is the placeholder or parameter in the () blocks 
     console.log("my name is yuvraj");
 }
