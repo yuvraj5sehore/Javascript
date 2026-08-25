79146cf9727d74f75c506b99568fdb2568968b4d
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-08-25 14:46:34 +0530

    added definition

--- Non primitive datatype/nonprimitive.js
+++ Non primitive datatype/nonprimitive.js
@@ -1,2 +1,2 @@
 // Non-primitive data types, also known as reference types, are objects and derived data types.
-//  They can store collections of values or more complex entities
+//  They can store collections of values or more complex entities.
