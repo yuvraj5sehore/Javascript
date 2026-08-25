da1b0c9a9db04f2bfd222c9cf997e22e3b98bd07
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-08-25 14:49:54 +0530

    object made

--- Non primitive datatype/nonprimitive.js
+++ Non primitive datatype/nonprimitive.js
@@ -1,2 +1,7 @@
 // Non-primitive data types, also known as reference types, are objects and derived data types.
 //  They can store collections of values or more complex entities.
+
+
+let obj = {};
+
+console.log(obj); // this will print empty {} which is called object 
