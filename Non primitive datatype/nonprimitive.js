6c56f5054a4cf9e4ca2a7d6e89c6f089b7e97668
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-08-25 14:43:37 +0530

    sd

--- Non primitive datatype/nonprimitive.js
+++ Non primitive datatype/nonprimitive.js
@@ -1,3 +1,2 @@
-// hello my name is yuvraj
-
-
+// Non-primitive data types, also known as reference types, are objects and derived data types.
+//  They can store collections of values or more complex entities
