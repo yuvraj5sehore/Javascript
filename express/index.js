15292fa676bd05aa9ae71719bd69021ac218984d
Yuvraj Thakur <yuvraj5sehore@gmail.com>
2026-09-25 22:03:35 +0530

    project started

--- express/index.js
+++ express/index.js
@@ -1,18 +1,35 @@
 const express = require("express");
-
 const app = express();
 
-console.dir(app);
-let port = 8080;
+const port = 8080;
 
-
-app.listen(port,()=>{
-  console.log(`The server is listening to ${port}`);
+app.listen(port, () => {
+  console.log(`The server is listening on port ${port}`);
+});
+// Specific routes
+app.get("/", (req, res) => {
+  res.send("You contacted root path");
 });
 
-app.use((req,res)=>{
-    console.log("request recieved");
-    let code = "<h1>Fruits</h1> <ul><li>apple</li> <li>orange</li></ul>";
-    res.send(code);
-})
+app.get("/apple", (req, res) => {
+  res.send("You contacted apple path");
+});
 
+app.get("/mango", (req, res) => {
+  res.send("You contacted mango path");
+});
+
+// Wildcard route for 404 / non-existent paths (MUST BE AT THE END)
+app.post("/",(req,res)=>{
+    res.send("you send a post request to root");
+});
+
+//path parameter
+app.get("/:username/:id", (req, res) => {
+    let{username,id}=req.params;
+    let htmlstr = `<h1>you contacted the page of @ ${username}!</h1>`;
+    res.send(htmlstr);
+});
+
+
+// Start the server
